400e655882153f2c36a2a6f4f64edb68996188ac
Arcadii Rubailo <rubailo.arcadii@gmail.com>
2018-02-06 23:33:38 +0100

    dd-3 Redesign add meal dialog

--- app/src/main/java/io/krugosvet/dailydish/android/DialogAddMeal.kt
+++ app/src/main/java/io/krugosvet/dailydish/android/DialogAddMeal.kt
@@ -1,44 +1,42 @@
 package io.krugosvet.dailydish.android
 
 import android.os.Bundle
-import android.support.v4.app.DialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams
-import android.widget.TextView
+import io.krugosvet.dailydish.android.utils.BaseDialogFragment
 import io.krugosvet.dailydish.android.utils.showKeyboard
 import kotlinx.android.synthetic.main.dialog_add_meal.*
-import kotterknife.bindView
 
-class DialogAddMeal : DialogFragment() {
-
-    private val addMealButton by bindView<TextView>(R.id.add_meal_button)
+class DialogAddMeal : BaseDialogFragment() {
 
     interface DialogAddMealListener {
         fun onAddButtonClick(mealTitle: String, mealDescription: String)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View
-            = inflater.inflate(R.layout.dialog_add_meal, container)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View = inflater.inflate(R.layout.dialog_add_meal, container)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        showKeyboard(mealTitle)
-        error.subscribeToEditText(mealTitle, getString(R.string.dialog_add_meal_empty_title_toast))
+        showKeyboard(mealTitle.editText!!)
 
-        addMealButton.findViewById<TextView>(R.id.add_meal_button).setOnClickListener {
-            val mealTitleText = mealTitle.text.toString()
+        addMealButton.setOnClickListener {
+            val mealTitleText = mealTitle.editText?.text.toString()
+            val mealDescriptionText = mealDescription.editText?.text.toString()
 
-            if (!mealTitleText.isEmpty()) {
-                (activity as DialogAddMealListener).onAddButtonClick(mealTitleText, mealDescription.text.toString())
-                dismiss()
+            when {
+                mealTitleText.isEmpty() -> mealTitle.error = getString(R.string.dialog_add_meal_empty_title_error)
+                mealDescriptionText.isEmpty() -> mealDescription.error = getString(R.string.dialog_add_meal_empty_description_error)
+                mealTitleText.isEmpty() || mealDescriptionText.isEmpty() -> return@setOnClickListener
             }
+            (activity as DialogAddMealListener).onAddButtonClick(mealTitleText, mealDescriptionText)
+            dismiss()
         }
     }
 
     override fun onResume() {
         super.onResume()
-        dialog.window.setLayout(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+        dialog.window.setLayout(getDimension(R.dimen.dialog_add_meal_width).toInt(), LayoutParams.WRAP_CONTENT)
     }
 }
--- app/src/main/java/io/krugosvet/dailydish/android/MealListAdapter.kt
+++ app/src/main/java/io/krugosvet/dailydish/android/MealListAdapter.kt
@@ -4,13 +4,15 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import io.krugosvet.dailydish.android.db.objects.Meal
-import io.realm.OrderedRealmCollection
+import io.realm.Realm
 import io.realm.RealmRecyclerViewAdapter
+import io.realm.kotlin.deleteFromRealm
 
-class MealListAdapter(private val mealList: OrderedRealmCollection<Meal>)
-    : RealmRecyclerViewAdapter<Meal, MealListAdapter.MealViewHolder>(mealList,true) {
+class MealListAdapter(private val realm: Realm)
+    : RealmRecyclerViewAdapter<Meal, MealListAdapter.MealViewHolder>(realm.where(Meal::class.java).findAll(),true) {
 
    init {
        setHasStableIds(true)
@@ -20,16 +22,20 @@ class MealListAdapter(private val mealList: OrderedRealmCollection<Meal>)
             MealViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_meal, parent, false))
 
     override fun onBindViewHolder(holder: MealViewHolder, position: Int) {
-        holder.bind(mealList[position])
+        holder.bind(data?.get(position))
     }
 
     inner class MealViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val title = view.findViewById<TextView>(R.id.title)
         private val description = view.findViewById<TextView>(R.id.description)
+        private val deleteButton = view.findViewById<Button>(R.id.delete)
 
-        fun bind(meal: Meal) {
-            title.text = meal.title
-            description.text = meal.description
+        fun bind(meal: Meal?) {
+            title.text = meal?.title
+            description.text = meal?.description
+            deleteButton.setOnClickListener {
+                realm.executeTransaction { meal?.deleteFromRealm() }
+            }
         }
     }
 }
