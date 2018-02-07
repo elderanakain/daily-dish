393bc9e7a1687a5cc9d15f64a86a2ee3b499ab86
Arcadii Rubailo <rubailo.arcadii@gmail.com>
2018-02-07 16:53:58 +0100

    dd-3 Fix add meal forms' errors

--- app/src/main/java/io/krugosvet/dailydish/android/DialogAddMeal.kt
+++ app/src/main/java/io/krugosvet/dailydish/android/DialogAddMeal.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewGroup.LayoutParams
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import io.krugosvet.dailydish.android.utils.BaseDialogFragment
+import io.krugosvet.dailydish.android.utils.BaseTextInputLayout
+import io.krugosvet.dailydish.android.utils.SimpleTextWatcher
 import io.krugosvet.dailydish.android.utils.showKeyboard
 import kotlinx.android.synthetic.main.dialog_add_meal.*
 
@@ -15,28 +17,48 @@ class DialogAddMeal : BaseDialogFragment() {
         fun onAddButtonClick(mealTitle: String, mealDescription: String)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View = inflater.inflate(R.layout.dialog_add_meal, container)
+    private val forms = mutableListOf<BaseTextInputLayout>()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View
+            = inflater.inflate(R.layout.dialog_add_meal, container)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        showKeyboard(mealTitle.editText!!)
+        showKeyboard(title.editText!!)
+        handleForms()
 
         addMealButton.setOnClickListener {
-            val mealTitleText = mealTitle.editText?.text.toString()
-            val mealDescriptionText = mealDescription.editText?.text.toString()
-
-            when {
-                mealTitleText.isEmpty() -> mealTitle.error = getString(R.string.dialog_add_meal_empty_title_error)
-                mealDescriptionText.isEmpty() -> mealDescription.error = getString(R.string.dialog_add_meal_empty_description_error)
-                mealTitleText.isEmpty() || mealDescriptionText.isEmpty() -> return@setOnClickListener
+            if (areFormsValid()) {
+                (activity as DialogAddMealListener)
+                        .onAddButtonClick(title.getEditTextInput(), description.getEditTextInput())
+                dismiss()
             }
-            (activity as DialogAddMealListener).onAddButtonClick(mealTitleText, mealDescriptionText)
-            dismiss()
         }
     }
 
     override fun onResume() {
         super.onResume()
-        dialog.window.setLayout(getDimension(R.dimen.dialog_add_meal_width).toInt(), LayoutParams.WRAP_CONTENT)
+        dialog.window.setLayout(getDimension(R.dimen.dialog_add_meal_width).toInt(), WRAP_CONTENT)
+    }
+
+    private fun handleForms() {
+        forms.addAll(listOf(title, description))
+        forms.forEach {
+            it.addTextChangedListener(object : SimpleTextWatcher {
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    if (s.isNullOrBlank()) it.error = getString(R.string.dialog_add_meal_empty_form_error, it.tag)
+                    else it.isErrorEnabled = false
+                }
+            })
+        }
+    }
+
+    private fun areFormsValid(): Boolean {
+        var isValid = true
+        forms.forEach {
+            it.triggerTextWatcher()
+            if (it.isErrorEnabled && isValid) isValid = false
+        }
+        return isValid
     }
 }
