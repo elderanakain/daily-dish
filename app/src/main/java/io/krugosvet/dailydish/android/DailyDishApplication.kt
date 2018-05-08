6b7fac7760b576f3697359ea25c17cf423568096
Arcadii Rubailo <rubailo.arcadii@gmail.com>
2018-05-08 22:39:01 +0200

    dd-13 Add AppID SDK

--- app/src/main/java/io/krugosvet/dailydish/android/DailyDishApplication.kt
+++ app/src/main/java/io/krugosvet/dailydish/android/DailyDishApplication.kt
@@ -1,6 +1,7 @@
 package io.krugosvet.dailydish.android
 
 import android.app.Application
+import com.ibm.bluemix.appid.android.api.AppID
 import io.realm.Realm
 
 class DailyDishApplication: Application() {
@@ -8,5 +9,6 @@ class DailyDishApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
+        AppID.getInstance().initialize(this, resources.getString(R.string.authTenantId), AppID.REGION_UK);
     }
 }
