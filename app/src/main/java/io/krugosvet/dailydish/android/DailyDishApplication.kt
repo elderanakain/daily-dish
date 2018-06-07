bafd87e91f07c13b1dfb033ef2598af782bb6f16
Arcadii Rubailo <rubailo.arcadii@gmail.com>
2018-06-07 17:21:57 +0200

    Server synchronization (#19)
    * dd-16 Add OkHttp + Retrofit dependecies
    
    * dd-16 Add Gson dependecy
    
    * dd-16 Extract Dagger modules
    
    * dd-16 AuthTokenManager abstraction
    
    * dd-16 MealServicePipe abstraction
    
    * dd-16 Dagger refactor
    
    * dd-16 Move Realm init to Dagger
    
    * dd-16 Add GET method to MealService
    
    * dd-16 Separate modules into classes
    
    * dd-16 Rename Component to AppComponent

--- app/src/main/java/io/krugosvet/dailydish/android/DailyDishApplication.kt
+++ app/src/main/java/io/krugosvet/dailydish/android/DailyDishApplication.kt
@@ -1,38 +1,27 @@
 package io.krugosvet.dailydish.android
 
-import android.app.Activity
 import android.app.Application
-import android.support.v4.app.Fragment
 import com.crashlytics.android.Crashlytics
-import com.ibm.bluemix.appid.android.api.AppID
-import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasActivityInjector
-import dagger.android.support.HasSupportFragmentInjector
 import io.fabric.sdk.android.Fabric
-import io.krugosvet.dailydish.android.dagger.BaseActivityModule
-import io.krugosvet.dailydish.android.dagger.DaggerDailyDishApplicationComponent
-import io.realm.Realm
-import javax.inject.Inject
+import io.krugosvet.dailydish.android.dagger.*
+import io.krugosvet.dailydish.android.dagger.module.AccountModule
+import io.krugosvet.dailydish.android.dagger.module.AppModule
+import io.krugosvet.dailydish.android.dagger.module.NetworkModule
 
-class DailyDishApplication: Application(), HasActivityInjector, HasSupportFragmentInjector {
+class DailyDishApplication : Application() {
 
-    @Inject
-    lateinit var dispatchingActivityInjector: DispatchingAndroidInjector<Activity>
-    @Inject
-    lateinit var dispatchingFragmentInjector: DispatchingAndroidInjector<Fragment>
+    companion object {
+        lateinit var appComponent: AppComponent
+    }
 
     override fun onCreate() {
         super.onCreate()
-        DaggerDailyDishApplicationComponent.builder()
-                .baseActivityModule(BaseActivityModule(this))
-                .build().inject(this)
-        Realm.init(this)
         Fabric.with(this, Crashlytics())
-        AppID.getInstance().initialize(this, resources.getString(R.string.authTenantId), AppID.REGION_UK)
+        appComponent = buildComponent()
     }
 
-    override fun activityInjector(): AndroidInjector<Activity>? = dispatchingActivityInjector
-
-    override fun supportFragmentInjector(): AndroidInjector<Fragment> = dispatchingFragmentInjector
+    private fun buildComponent() = DaggerAppComponent.builder()
+            .appModule(AppModule(this))
+            .accountModule(AccountModule())
+            .networkModule(NetworkModule()).build()
 }
