d9b1630446d5f4254aa3905f4aebcce7ea23db2b
Arcadii Rubailo <rubailo.arcadii@gmail.com>
2020-10-05 22:30:21 +0200

    Refactor image service Closes #48

--- app/src/main/java/io/krugosvet/dailydish/android/service/ImagePickerService.kt
+++ app/src/main/java/io/krugosvet/dailydish/android/service/ImagePickerService.kt
@@ -1,14 +1,12 @@
 package io.krugosvet.dailydish.android.service
 
-import android.Manifest
+import android.Manifest.permission.CAMERA
 import android.app.AlertDialog
 import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.annotation.StringRes
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.DefaultLifecycleObserver
-import androidx.lifecycle.LifecycleOwner
 import com.mlsdev.rximagepicker.RxImagePicker
 import com.mlsdev.rximagepicker.Sources
 import io.krugosvet.dailydish.android.R
@@ -19,12 +17,19 @@ import io.krugosvet.dailydish.android.service.ImagePickerService.DialogSource.Ac
 import io.krugosvet.dailydish.android.service.ImagePickerService.DialogSource.Action.Update
 import io.krugosvet.dailydish.android.service.ImagePickerService.DialogSource.Source
 import io.krugosvet.dailydish.core.service.ResourceService
-import io.reactivex.Observable
-import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.sendBlocking
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.drop
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.rx2.asFlow
+import java.util.concurrent.CancellationException
 
-const val REQUEST_PERMISSION_CAMERA = 2
+private const val REQUEST_PERMISSION_CAMERA = 2
 
 class ImagePickerService(
   private val activity: GenericBaseActivity,
@@ -44,87 +49,69 @@ class ImagePickerService(
     }
   }
 
-  private var pickerDisposable: Disposable? = null
-
-  private val isCameraPermissionGranted
-    get() = ContextCompat.checkSelfPermission(activity, Manifest.permission.CAMERA) ==
+  private val isCameraPermissionGranted: Boolean
+    get() = ContextCompat.checkSelfPermission(activity, CAMERA) ==
       PackageManager.PERMISSION_GRANTED
 
-  init {
-    activity.lifecycle.addObserver(object : DefaultLifecycleObserver {
-
-      override fun onDestroy(owner: LifecycleOwner) {
-        super.onDestroy(owner)
-
-        pickerDisposable?.dispose()
-      }
-    })
+  fun showImagePicker(isImageEmpty: Boolean): Flow<Uri> = when {
+    isImageEmpty -> openImageProviderPicker()
+    else -> openActionDialog()
   }
 
-  fun showImagePicker(isImageEmpty: Boolean) =
-    if (isImageEmpty) {
-      openImageProviderChooser()
-    } else {
-      openActionDialog()
-    }
-
-  private fun openActionDialog(): Single<Uri> =
-    Single.just(true)
-      .observeOn(AndroidSchedulers.mainThread())
-      .showDialog(Action::class.sealedObjects.toTypedArray())
-      .flatMap {
-        when (it) {
-          Update -> openImageProviderChooser()
-          Remove -> Single.just(Uri.EMPTY)
+  private fun openActionDialog(): Flow<Uri> =
+    showDialog(Action::class.sealedObjects.toTypedArray())
+      .flatMapConcat { source ->
+        when (source) {
+          Update -> openImageProviderPicker()
+          Remove -> flowOf(Uri.EMPTY)
         }
       }
 
-  private fun getImageFromCamera(): Single<Uri> =
-    if (isCameraPermissionGranted) {
-      RxImagePicker.with(activity).requestImage(Sources.CAMERA).firstOrError()
-    } else {
-      ActivityCompat.requestPermissions(
-        activity, arrayOf(Manifest.permission.CAMERA), REQUEST_PERMISSION_CAMERA
-      )
-
-      activity.permissionsObservable
-        .flatMap {
-          if (isCameraPermissionGranted) {
-            RxImagePicker.with(activity).requestImage(Sources.CAMERA)
-          } else {
-            Observable.error(IllegalStateException("Permission is not granted"))
-          }
+  private fun getImageFromCamera(): Flow<Uri> =
+    activity.permissionsObservable
+      .drop(when {
+        isCameraPermissionGranted -> 0
+        else -> {
+          requestCameraPermission(); 1
         }
-        .firstOrError()
-    }
+      })
+      .flatMapLatest {
+        RxImagePicker.with(activity)
+          .requestImage(Sources.CAMERA)
+          .asFlow()
+      }
 
-  private fun openImageProviderChooser(): Single<Uri> =
-    Single.just(true)
-      .observeOn(AndroidSchedulers.mainThread())
-      .showDialog(Source::class.sealedObjects.toTypedArray())
-      .flatMap {
-        when (it) {
+  private fun requestCameraPermission() {
+    ActivityCompat.requestPermissions(activity, arrayOf(CAMERA), REQUEST_PERMISSION_CAMERA)
+  }
+
+  private fun openImageProviderPicker(): Flow<Uri> =
+    showDialog(Source::class.sealedObjects.toTypedArray())
+      .flatMapConcat { source ->
+        when (source) {
           Source.Camera -> getImageFromCamera()
           Source.Gallery -> RxImagePicker.with(activity)
             .requestImage(Sources.GALLERY)
-            .firstOrError()
+            .asFlow()
         }
       }
 
-  private fun <T : DialogSource> Single<*>.showDialog(source: Array<T>) =
-    flatMap {
-      Single.create<T> { emitter ->
-        AlertDialog.Builder(activity)
-          .setItems(source.map { resources.getString(it.text) }.toTypedArray()) { dialog, which ->
-            emitter.onSuccess(source[which])
-            dialog.dismiss()
-          }
-          .setNegativeButton(resources.getString(R.string.dialog_cancel_button)) { dialog, _ ->
-            emitter.onError(IllegalStateException("Dialog is closed"))
-            dialog.dismiss()
-          }
-          .create().show()
-      }
+  private fun <T : DialogSource> showDialog(source: Array<T>): Flow<T> =
+    callbackFlow {
+      val dialog = AlertDialog.Builder(activity)
+        .setItems(source.map { resources.getString(it.text) }.toTypedArray()) { dialog, which ->
+          sendBlocking(source[which])
+          dialog.dismiss()
+        }
+        .setNegativeButton(resources.getString(R.string.dialog_cancel_button)) { dialog, _ ->
+          cancel(CancellationException("Dialog is closed"))
+          dialog.dismiss()
+        }
+        .create()
+
+      dialog.show()
+
+      awaitClose { dialog.dismiss() }
     }
 
 }
