fb0cf2107f202cbd8183cf715611edb94b5f9bb7
elderanakain <elderanakain@users.noreply.github.com>
2023-11-02 23:23:01 +0100

    Add linker flags [published]

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://maven.pkg.github.com/elderanakain/daily-dish/io/krugosvet/dailydish/common-kmmbridge/1.3.1-SNAPSHOT/common-kmmbridge-1.3.1-20231102.221444-11.zip"
-let remoteKotlinChecksum = "1cea344be478cd71ece6cdccaed0d7b04ea7a5c4c00bd5d8290492fb5bdac84b"
+let remoteKotlinUrl = "https://maven.pkg.github.com/elderanakain/daily-dish/io/krugosvet/dailydish/common-kmmbridge/1.3.1-SNAPSHOT/common-kmmbridge-1.3.1-20231102.223237-14.zip"
+let remoteKotlinChecksum = "cfec61f3e7efdcd48ae654f9304e5f57d0aeeb89eeb7f53b3d4ebc23750e6e43"
 let packageName = "DDCore"
 // END KMMBRIDGE BLOCK
 
