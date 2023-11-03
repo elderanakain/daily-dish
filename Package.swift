719eeb8cb60b534b98dd5d2fc0c6cc44be1428cc
elderanakain <elderanakain@users.noreply.github.com>
2023-11-03 17:20:49 +0100

    Bump up version [published]

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://maven.pkg.github.com/elderanakain/daily-dish/io/krugosvet/dailydish/common-kmmbridge/1.3.1/common-kmmbridge-1.3.1.zip"
-let remoteKotlinChecksum = "6f95f47a13b694ea988730e568c5708b8b918ad3eafa376696269f314a10b310"
+let remoteKotlinUrl = "https://maven.pkg.github.com/elderanakain/daily-dish/io/krugosvet/dailydish/common-kmmbridge/1.4.0-SNAPSHOT/common-kmmbridge-1.4.0-20231103.162129-1.zip"
+let remoteKotlinChecksum = "88d95611f718dab4e0751ed625e074f386fe2a62a76a0d5c155ec9a70775a533"
 let packageName = "DDCore"
 // END KMMBRIDGE BLOCK
 
