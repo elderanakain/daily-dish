a64fecec4536a382a43f67d4d9db12f1b4a9312d
elderanakain <elderanakain@users.noreply.github.com>
2024-02-20 01:43:19 +0200

    Fix create tag output [published]

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://maven.pkg.github.com/elderanakain/daily-dish/io/krugosvet/dailydish/common-kmmbridge/1.4.1-SNAPSHOT/common-kmmbridge-1.4.1-20240219.232242-5.zip"
-let remoteKotlinChecksum = "d37a77f2d5396bac5d4542c5cc0be72efd82ed1c7afb09a2a05e0ff8536b0c74"
+let remoteKotlinUrl = "https://maven.pkg.github.com/elderanakain/daily-dish/io/krugosvet/dailydish/common-kmmbridge/1.4.1-SNAPSHOT/common-kmmbridge-1.4.1-20240219.234358-6.zip"
+let remoteKotlinChecksum = "e9d6cc3b5a7e862f2fa0dec668d0c68cc43d4ad7fe65a9f85b9e0464d67c0e3d"
 let packageName = "DDCore"
 // END KMMBRIDGE BLOCK
 
