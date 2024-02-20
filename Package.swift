6948650f9eac965a2be15fddf440f90143ccd352
elderanakain <elderanakain@users.noreply.github.com>
2024-02-20 12:16:13 +0200

    Fix BE tests [published]

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://maven.pkg.github.com/elderanakain/daily-dish/io/krugosvet/dailydish/common-kmmbridge/1.4.2-SNAPSHOT/common-kmmbridge-1.4.2-20240220.100358-3.zip"
-let remoteKotlinChecksum = "7ff83fcc4631db18a4b9d5f95c5bdcd0f0531a8a1d7795eb893b38cacc5116d9"
+let remoteKotlinUrl = "https://maven.pkg.github.com/elderanakain/daily-dish/io/krugosvet/dailydish/common-kmmbridge/1.4.2-SNAPSHOT/common-kmmbridge-1.4.2-20240220.101747-4.zip"
+let remoteKotlinChecksum = "8acfdcf29aca865a6c8a0780fd24deadd184c6c293266839db0c5a36fd1c6bdd"
 let packageName = "DDCore"
 // END KMMBRIDGE BLOCK
 
